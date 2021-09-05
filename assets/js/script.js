08adef19e4e132952bec2a9953caff8f91896a93
mebelekeziak <requsziqq@gmail.com>
2021-09-05 10:59:39 +0200

    strona

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+document.getElementById("Download").onclick = function () {
+    location.href = "app.html";
+};
 document.getElementById("przycisk").onclick = function () {
     location.href = "app.html";
 };
